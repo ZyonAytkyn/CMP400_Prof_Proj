711a5007ce3850ea74a2f8cd97e50c68ab1db176
ZyonAytkyn <zionaitken@gmail.com>
2025-02-03 20:48:45 +0000

    The project works again!!!

--- Source/CMP400_Prof_Proj/TP_WeaponComponent.cpp
+++ Source/CMP400_Prof_Proj/TP_WeaponComponent.cpp
@@ -18,6 +18,9 @@ UTP_WeaponComponent::UTP_WeaponComponent()
 {
 	// Default offset from the character location for projectiles to spawn
 	MuzzleOffset = FVector(100.0f, 0.0f, 10.0f);
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Some debug message!"));
+	}
 }
 
 
